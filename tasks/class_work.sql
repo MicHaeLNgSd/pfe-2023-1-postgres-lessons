45524eab6213496cded42bea2e4b5888c1a55d83
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-03-08 16:32:29 +0200

    add variants of last ex

--- tasks/class_work.sql
+++ tasks/class_work.sql
@@ -25,7 +25,7 @@ FROM orders o
 JOIN products_to_orders pto ON o.id = pto.order_id
 JOIN products p ON p.id = pto.product_id
 WHERE o.status SIMILAR TO 'finished' AND o.user_id = 3
-GROUP BY o.id
+GROUP BY o.id;
 
 --* Дані про найпопулярніший товар (товар який є в найбільшій кількості унікальних замовлень)
 SELECT p.*, count(pto.product_id) buying_quantity
@@ -33,7 +33,7 @@ FROM products p
 JOIN products_to_orders pto ON p.id = pto.product_id
 GROUP BY p.id
 ORDER BY buying_quantity DESC
-LIMIT 1
+LIMIT 1;
 
 --* Всі замовлення, кінцева вартість яких більше за середню вартість замовлення (підзапити)
 WITH orders_with_full_price as (
@@ -50,4 +50,36 @@ average_order_price as (
 SELECT o_fp.*
 FROM orders_with_full_price o_fp, average_order_price avg_op
 WHERE o_fp.full_price > avg_op.avg_price
-ORDER BY o_fp.full_price
+ORDER BY o_fp.full_price;
+
+--
+WITH avg_table AS (
+  SELECT avg(pto.quantity * p.price) "avg_of_all"
+  FROM products "p"
+  JOIN products_to_orders "pto" ON p.id = pto.product_id
+  JOIN orders "o" ON pto.order_id = o.id
+)
+SELECT avg(pto.quantity * p.price), o.*
+FROM products "p"
+  JOIN products_to_orders "pto" ON p.id = pto.product_id
+  JOIN orders "o" ON pto.order_id = o.id
+  JOIN avg_table "avgt" ON 1=1
+GROUP BY o.id, avgt.avg_of_all
+HAVING avg(pto.quantity * p.price) > avgt.avg_of_all
+ORDER BY o.id;
+
+--
+WITH avg_price as (
+  SELECT avg(p.price * pto.quantity) price
+  FROM orders o
+  JOIN products_to_orders pto ON o.id = pto.order_id
+  JOIN products p ON p.id = pto.product_id
+)
+SELECT p.price * pto.quantity full_price, o.*
+FROM orders o
+JOIN products_to_orders pto ON o.id = pto.order_id
+JOIN products p ON p.id = pto.product_id
+JOIN avg_price avgp ON 1=1
+WHERE p.price * pto.quantity > avgp.price
+GROUP BY o.id, p.price, pto.quantity, avgp.price
+ORDER BY avgp.price;
